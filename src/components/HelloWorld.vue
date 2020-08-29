ee41498caa77369a837e39cb0b53ba2b095c637a
daisuzuki829 <dai.suzuki.829@gmail.com>
2020-08-29 23:00:02 +0900

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,4 @@
+<!-- 
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
@@ -94,7 +95,36 @@ export default {
   }
 };
 </script>
+ -->
 
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <p>{{ total }}</p>
+    <!-- incrementと言うイベントを定義。子コンポーネントから$emitされたら、incrementTotal実行される -->
+    <hello-child v-on:increment="incrementTotal" />
+  </div>
+</template>
+
+<script>
+import HelloChild from "./HelloChild";
+export default {
+  data: function() {
+    return {
+      title: "はろー！わーるど！",
+      total: 0
+    };
+  },
+  methods: {
+    incrementTotal: function() {
+      this.total += 1;
+    }
+  },
+  components: {
+    HelloChild
+  }
+};
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
